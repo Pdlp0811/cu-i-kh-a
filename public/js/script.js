6aa311e2631f8b1f0e9c510f14421c37b658542c
Le Phuoc <lephuoc08112008@gmail.com>
2024-11-24 16:09:29 +0700

    Deployed web

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,11 @@
 cog = document.querySelector(".system");
-
 cog.addEventListener("click", function () {
   console.log("click");
   document.querySelector(".sidebar").classList.toggle("side");
 });
 
-cog = document.querySelector(".menu1");
-
-cog.addEventListener("click", function () {
+menu = document.querySelector(".menu1");
+menu.addEventListener("click", function () {
   console.log("click");
-  document.querySelector(".menu2").classList.toggle("home");
+  document.querySelector(".menu2").classList.toggle("menu3");
 });
