6b2b015b2de384eddcfd4313466c428f133d9d7b
Le Phuoc <lephuoc08112008@gmail.com>
2024-12-07 21:10:09 +0700

    cog

--- public/js/script.js
+++ public/js/script.js
@@ -9,3 +9,19 @@ menu.addEventListener("click", function () {
   console.log("click");
   document.querySelector(".menu2").classList.toggle("menu3");
 });
+
+dark = document.querySelector(".dark");
+dark.addEventListener("click", function () {
+  console.log("click");
+  document.querySelector(".l").classList.add("l1");
+  document.querySelector(".light").classList.add("light1");
+  document.querySelector(".dark").classList.add("dark1");
+});
+
+light = document.querySelector(".light");
+light.addEventListener("click", function () {
+  console.log("click");
+  document.querySelector(".l").classList.remove("l1");
+  document.querySelector(".light").classList.remove("light1");
+  document.querySelector(".dark").classList.remove("dark1");
+});
